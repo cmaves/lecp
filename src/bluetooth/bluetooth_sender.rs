f34f46b7284279b64dcb01d3a4d6d7855a694f51
Curtis Maves <curtismaves@gmail.com>
2020-08-26 15:40:30 -0500

    Added periodic time notifications

--- src/bluetooth/bluetooth_sender.rs
+++ src/bluetooth/bluetooth_sender.rs
@@ -4,7 +4,7 @@ use rustable::gatt::{
     CharFlags, CharValue, Characteristic, LocalCharBase, LocalServiceBase, Service,
 };
 use rustable::{Bluetooth as BT, Device, ToUUID, ValOrFn, UUID};
-use std::cell::RefCell;
+use std::cell::{Cell, RefCell};
 use std::rc::Rc;
 use std::sync::mpsc::{sync_channel, Receiver, SyncSender, TryRecvError};
 use std::thread::{sleep, spawn, JoinHandle};
@@ -14,8 +14,8 @@ const ECP_TIME: &'static str = "79f4bb2c-7885-4584-8ef9-ae205b0eb345";
 
 struct Bluetooth {
     blue: BT,
-    time: u32,
-    last_set: Instant,
+    time: Rc<Cell<u32>>,
+    last_set: Rc<Cell<Instant>>,
     msgs: Rc<RefCell<[Option<LedMsg>; 256]>>,
 }
 
@@ -26,8 +26,8 @@ impl Bluetooth {
         blue.verbose = verbose;
         let mut ret = Bluetooth {
             blue,
-            time: 0,
-            last_set: Instant::now(),
+            time: Rc::new(Cell::new(0)),
+            last_set: Rc::new(Cell::new(Instant::now())),
             msgs: Rc::new(RefCell::new([None; 256])),
         };
         ret.init_service()?;
@@ -71,17 +71,33 @@ impl Bluetooth {
             let mut ecp_char = sender_service.get_char(uuid).unwrap();
             ecp_char.write_val_or_fn(&mut ValOrFn::Function(Box::new(read_fn)));
         }
+        let time = self.time.clone();
+        let last_set = self.last_set.clone();
+        let time_closure = move || {
+            time_fn(time.get(), last_set.get(), Instant::now())
+                .to_be_bytes()
+                .as_ref()
+                .into()
+        };
+        let mut time_serv = sender_service.get_char(&uuids[5]).unwrap();
+        time_serv.write_val_or_fn(&mut ValOrFn::Function(Box::new(time_closure)));
         self.blue.register_application()?;
         Ok(())
     }
     fn cur_time(&self) -> u32 {
-        self.time
-            .wrapping_add(Instant::now().duration_since(self.last_set).as_micros() as u32)
+        self.time.get().wrapping_add(
+            Instant::now()
+                .duration_since(self.last_set.get())
+                .as_micros() as u32,
+        )
     }
     fn process_requests(&mut self) -> Result<(), Error> {
         self.blue.process_requests()?;
         Ok(())
     }
+}
+fn time_fn(time: u32, last_set: Instant, now: Instant) -> u32 {
+    time.wrapping_add(now.duration_since(last_set).as_micros() as u32)
 }
 pub struct BluetoothSender {
     sender: SyncSender<BMsg>,
@@ -93,6 +109,7 @@ impl BluetoothSender {
         let handle = Status::Running(spawn(move || {
             let mut bt = Bluetooth::new(blue_path, verbose)?;
             let ecp_bufs = ecp_bufs();
+            let mut last_notify_time = Instant::now();
             loop {
                 bt.process_requests()?;
                 match recv.try_recv() {
@@ -101,10 +118,15 @@ impl BluetoothSender {
                             if msgs.len() == 0 {
                                 continue;
                             }
-                            bt.last_set = Instant::now();
-                            bt.time = msgs[0]
-                                .cur_time
-                                .wrapping_add(bt.last_set.duration_since(start).as_micros() as u32);
+                            let now = Instant::now();
+                            let old_time = time_fn(bt.time.get(), bt.last_set.get(), now);
+                            let cur_time = time_fn(msgs[0].cur_time, start, now);
+
+                            bt.last_set.set(now);
+                            bt.time.set(cur_time);
+                            let notify_time = (cur_time.wrapping_sub(old_time) as i32).abs()
+                                > 5_000
+                                || now.duration_since(last_notify_time).as_secs() > 5_000;
 
                             let mut mut_msgs = bt.msgs.borrow_mut();
                             for msg in &msgs {
@@ -113,7 +135,8 @@ impl BluetoothSender {
                             for msg in mut_msgs.iter_mut() {
                                 // prune old messages
                                 if let Some(v) = msg {
-                                    if (bt.time.wrapping_sub(v.cur_time) as i32).abs() > 5_000_000 {
+                                    if (cur_time.wrapping_sub(v.cur_time) as i32).abs() > 5_000_000
+                                    {
                                         // the i32::abs allows values that are up to 5 seconds early to array
                                         *msg = None;
                                     }
@@ -124,6 +147,11 @@ impl BluetoothSender {
                             // eprintln!("dirty received: {:?}", dirty);
                             // write out the dirty characteristics and
                             let mut service = bt.blue.get_service(ECP_UUID).unwrap();
+                            if notify_time {
+                                let mut character = service.get_char(ECP_TIME).unwrap();
+                                character.notify(None)?;
+                                last_notify_time = now;
+                            }
                             let mut notify_char = service.get_char(&ecp_bufs[0]).unwrap();
                             let mtu = notify_char.notify_mtu().unwrap_or(23) - 3; // The 3 accounts for ATT HDR
                             let mut written = 0;
@@ -141,39 +169,6 @@ impl BluetoothSender {
                                         return Err(e.into());
                                     }
                                 }
-                            }
-                            let cur_time = bt.time;
-                            let last_set = bt.last_set;
-                            let time_fn = move || {
-                                let mut ret = CharValue::default();
-                                ret.extend_from_slice(
-                                    &cur_time
-                                        .wrapping_add(
-                                            Instant::now().duration_since(last_set).as_micros()
-                                                as u32,
-                                        )
-                                        .to_be_bytes(),
-                                );
-                                ret
-                            };
-                            let mut character = service.get_char(ECP_TIME).unwrap();
-                            let new_time = time_fn();
-                            let mut val = ValOrFn::Function(Box::new(time_fn));
-                            character.write_val_or_fn(&mut val);
-                            let old_time = val.to_value();
-                            if old_time.len() == 4 {
-                                debug_assert_eq!(4, new_time.len());
-                                let mut new_buf = [0; 4];
-                                let mut old_buf = [0; 4];
-                                new_buf.copy_from_slice(&new_time[..4]);
-                                old_buf.copy_from_slice(&old_time[..4]);
-                                let new_time = i32::from_be_bytes(new_buf);
-                                let old_time = i32::from_be_bytes(old_buf);
-                                if new_time.wrapping_sub(old_time).abs() > 5000 {
-                                    character.notify(None)?;
-                                }
-                            } else {
-                                character.notify(None)?;
                             }
                         }
                         BMsg::Terminate => return Ok(()),
